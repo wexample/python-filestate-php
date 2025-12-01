2be0a35789aee631e57e967e88dd1ca6d6f3fee7
weeger <romain.weeger@gmail.com>
2025-12-01 17:45:01 +0100

    PhpCs option

---
+++ src/wexample_filestate_php/options_provider/php_options_provider.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from wexample_config.options_provider.abstract_options_provider import (
+    AbstractOptionsProvider,
+)
+
+if TYPE_CHECKING:
+    from wexample_config.config_option.abstract_config_option import (
+        AbstractConfigOption,
+    )
+
+
+class PhpOptionsProvider(AbstractOptionsProvider):
+    @classmethod
+    def get_options(cls) -> list[type[AbstractConfigOption]]:
+        from wexample_filestate_php.option.php_option import PhpOption
+
+        return [
+            PhpOption,
+        ]
